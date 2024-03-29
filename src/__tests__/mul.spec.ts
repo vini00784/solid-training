c88bc823825eb6b51fde9780b5f8c9b84f273d92
vini00784 <vo70346@gmail.com>
2024-03-29 17:58:04 -0300

    tests for multiply function

---
+++ src/__tests__/mul.spec.ts
@@ -0,0 +1,39 @@
+import { it, describe, expect } from "vitest";
+import { mul } from "..";
+import {
+  correctMulMocks,
+  exactlyMulResultMocks,
+  expectMulErrorMocks,
+  notNegativeMulResultMocks,
+} from "./__mocks__/mul.mock";
+
+describe("Testing multiply function", () => {
+  for (const mockData of correctMulMocks) {
+    it("Should multiply a * b", () => {
+      const result = mul(mockData.a, mockData.b);
+      expect(result).toEqual(expect.any(Number));
+    });
+  }
+
+  for (const mockData of exactlyMulResultMocks) {
+    it("Should return result equal 20", () => {
+      const result = mul(mockData.a, mockData.b);
+      expect(result).toStrictEqual(20);
+    });
+  }
+
+  for (const mockData of notNegativeMulResultMocks) {
+    it("Should not return negative result", () => {
+      const result = mul(mockData.a, mockData.b);
+      expect(result).toBeGreaterThan(0);
+    });
+  }
+
+  for (const mockData of expectMulErrorMocks) {
+    it("Should throw any exception", () => {
+      // @ts-expect-error
+      const result = mul(mockData.a, mockData.b);
+      expect(result).toThrow(expect.any(Error));
+    });
+  }
+});

effd01e6003525f876294c088c0adf1c56a83806
vini00784 <vo70346@gmail.com>
2024-03-29 18:38:18 -0300

    mock data for division function

---
+++ src/__tests__/__mocks__/div.mock.ts
@@ -0,0 +1,75 @@
+export const correctDivMocks = [
+  {
+    a: 25,
+    b: 5,
+  },
+  {
+    a: 66,
+    b: 11,
+  },
+  {
+    a: 109,
+    b: 18,
+  },
+  {
+    a: 16,
+    b: 4,
+  },
+];
+
+export const exactlyDivResultMocks = [
+  {
+    a: 88,
+    b: 2,
+  },
+  {
+    a: 44,
+    b: 1,
+  },
+  {
+    a: 22,
+    b: 0.5,
+  },
+  {
+    a: 440,
+    b: 10,
+  },
+];
+
+export const notNegativeDivResultMocks = [
+  {
+    a: -4,
+    b: -2,
+  },
+  {
+    a: 28,
+    b: 7,
+  },
+  {
+    a: 0,
+    b: 197,
+  },
+  {
+    a: 42,
+    b: 6,
+  },
+];
+
+export const expectDivErrorMocks = [
+  {
+    a: false,
+    b: false,
+  },
+  {
+    a: 12,
+    b: 0,
+  },
+  {
+    a: undefined,
+    b: "",
+  },
+  {
+    a: 0,
+    b: 0,
+  },
+];

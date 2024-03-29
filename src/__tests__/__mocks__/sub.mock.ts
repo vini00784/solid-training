714dfac7ec4134406cb33b6a1e661d02c3266c17
vini00784 <vo70346@gmail.com>
2024-03-29 17:29:23 -0300

    mock data for subtract function tests

---
+++ src/__tests__/__mocks__/sub.mock.ts
@@ -0,0 +1,75 @@
+export const correctSubMocks = [
+  {
+    a: 12,
+    b: 8,
+  },
+  {
+    a: -8,
+    b: 10,
+  },
+  {
+    a: 35,
+    b: 29,
+  },
+  {
+    a: 429,
+    b: 53,
+  },
+];
+
+export const exactlySubResultMocks = [
+  {
+    a: 20,
+    b: 16,
+  },
+  {
+    a: -147,
+    b: -151,
+  },
+  {
+    a: 4,
+    b: 0,
+  },
+  {
+    a: 12,
+    b: 8,
+  },
+];
+
+export const notNegativeSubResultMocks = [
+  {
+    a: 10,
+    b: 9,
+  },
+  {
+    a: -782,
+    b: -1100,
+  },
+  {
+    a: 0,
+    b: 0,
+  },
+  {
+    a: 187,
+    b: 76,
+  },
+];
+
+export const expectSubErrorMocks = [
+  {
+    a: "vasco da gama",
+    b: true,
+  },
+  {
+    a: "roger guedes maior artilheiro da arena",
+    b: 123,
+  },
+  {
+    a: 2,
+    b: false,
+  },
+  {
+    a: false,
+    b: false,
+  },
+];

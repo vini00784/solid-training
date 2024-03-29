46c2b628a0928ac6bc2cbc31affb2a8a202a0545
vini00784 <vo70346@gmail.com>
2024-03-29 15:48:41 -0300

    tests for sum function finished

--- src/__tests__/sum.spec.ts
+++ src/__tests__/sum.spec.ts
@@ -1,19 +1,39 @@
 import { it, describe, expect } from "vitest";
 import { sum } from "..";
+import {
+  correctSumMocks,
+  exactlySumResultMocks,
+  expectSumErrorMocks,
+  notNegativeSumResultMocks,
+} from "./__mocks__/sum.mock";
 
 describe("Testing sum function", () => {
-  it("Should sum a + b", () => {
-    const result = sum(3, 6);
-    expect(result).toEqual(expect.any(Number));
-  });
+  for (const mockData of correctSumMocks) {
+    it("Should sum a + b", () => {
+      const result = sum(mockData.a, mockData.b);
+      expect(result).toEqual(expect.any(Number));
+    });
+  }
 
-  it("Should return result equal 12", () => {
-    const result = sum(7, 5);
-    expect(result).toEqual(12);
-  });
+  for (const mockData of exactlySumResultMocks) {
+    it("Should return result equal 12", () => {
+      const result = sum(mockData.a, mockData.b);
+      expect(result).toEqual(12);
+    });
+  }
 
-  it("Should not return negative result", () => {
-    const result = sum(-9, 10);
-    expect(result).greaterThan(0);
-  });
+  for (const mockData of notNegativeSumResultMocks) {
+    it("Should not return negative result", () => {
+      const result = sum(mockData.a, mockData.b);
+      expect(result).greaterThanOrEqual(0);
+    });
+  }
+
+  for (const mockData of expectSumErrorMocks) {
+    it("Should throw any exception", () => {
+      // @ts-expect-error
+      const result = sum(mockData.a, mockData.b);
+      expect(result).toThrow(expect.any(Error));
+    });
+  }
 });

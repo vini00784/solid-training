88494becbd4093fbb9c2fed364705ae4c043ede4
vini00784 <vo70346@gmail.com>
2024-03-29 15:48:50 -0300

    mock data for sum function

---
+++ src/__tests__/__mocks__/sum.mock.ts
@@ -0,0 +1,75 @@
+export const correctSumMocks = [
+  {
+    a: 3,
+    b: 4,
+  },
+  {
+    a: 10,
+    b: 25,
+  },
+  {
+    a: 80,
+    b: 24,
+  },
+  {
+    a: 1,
+    b: 1,
+  },
+];
+
+export const exactlySumResultMocks = [
+  {
+    a: 5,
+    b: 7,
+  },
+  {
+    a: 4,
+    b: 8,
+  },
+  {
+    a: 10,
+    b: 2,
+  },
+  {
+    a: 0,
+    b: 12,
+  },
+];
+
+export const notNegativeSumResultMocks = [
+  {
+    a: -9,
+    b: 10,
+  },
+  {
+    a: -120,
+    b: 120,
+  },
+  {
+    a: 26,
+    b: -19,
+  },
+  {
+    a: 16,
+    b: -15,
+  },
+];
+
+export const expectSumErrorMocks = [
+  {
+    a: "eu to lek lek",
+    b: 2,
+  },
+  {
+    a: true,
+    b: false,
+  },
+  {
+    a: [2, 4, "todos"],
+    b: 19,
+  },
+  {
+    a: 269,
+    b: false,
+  },
+];
